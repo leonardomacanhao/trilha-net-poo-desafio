29bc357d8689f2e05e47a73104ba1dc94b43f747
João Leonardo Macanhão <leonardomacanhao@gmail.com>
2023-12-28 16:40:33 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone NOKIA:");
+Smartphone nokia = new Nokia(numero: "(11)99999-8888" , modelo: "M1" , imei: "123456789" , memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPHONE:");
+Smartphone iphone = new Iphone(numero: "(11)99999-7777" , modelo: "15" , imei: "542352253" , memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
